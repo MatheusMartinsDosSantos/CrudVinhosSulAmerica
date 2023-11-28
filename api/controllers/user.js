879b99e81aa9f9e154ee625ef6f662fed3a8d43a
MatheusMartinsDosSantos <matheus.msantos58@senacsp.edu.br>
2023-11-28 20:51:27 -0300

    Intial

---
+++ api/controllers/user.js
@@ -0,0 +1,55 @@
+import { db } from "../db.js";
+
+export const getUsers = (_, res) => {
+    const q = "SELECT * FROM tb_vinho";
+
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json(data);
+    });
+};
+
+export const addUser = (req, res) => {
+    const q =
+      "INSERT INTO tb_vinho(`nm_vinho`, `ps_vinho`, `pr_vinho`) VALUES(?)";
+  
+    const values = [
+      req.body.nm_vinho,
+      req.body.ps_vinho,
+      req.body.pr_vinho,
+    ];
+  
+    db.query(q, [values], (err) => {
+      if (err) return res.json(err);
+  
+      return res.status(200).json("Usuário criado com sucesso.");
+    });
+  };
+  
+  export const updateUser = (req, res) => {
+    const q =
+      "UPDATE tb_vinho SET `nm_vinho` = ?, `ps_vinho` = ?, `pr_vinho` = ? WHERE `id` = ?";
+  
+    const values = [
+      req.body.nm_vinho,
+      req.body.ps_vinho,
+      req.body.pr_vinho,
+    ];
+  
+    db.query(q, [...values, req.params.id], (err) => {
+      if (err) return res.json(err);
+  
+      return res.status(200).json("Usuário atualizado com sucesso.");
+    });
+  };
+  
+  export const deleteUser = (req, res) => {
+    const q = "DELETE FROM tb_vinho WHERE `id` = ?";
+  
+    db.query(q, [req.params.id], (err) => {
+      if (err) return res.json(err);
+  
+      return res.status(200).json("Usuário deletado com sucesso.");
+    });
+  };
---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import userRoutes from "./routes/users.js";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/login')
+
+app.use("/", userRoutes);
+
+app.listen(8800);
+
