03b8e77db55d189bbbbfc7f41d41e1d2452880a1
MatheusMartinsDosSantos <112398203+MatheusMartinsDosSantos@users.noreply.github.com>
2023-11-29 15:01:30 -0300

    Deploy Fix

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql"
 
 export const db = mysql.createConnection({
     host: "20.195.166.255",
-    user: "admin",
-    password: "@dm!n",
+    user: "root",
+    password: "P@$$w0rd",
     database: "senac2303",
 })
