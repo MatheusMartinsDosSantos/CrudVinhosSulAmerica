12b4a49f0526ee8377d65c241f172373fadf8ad1
MatheusMartinsDosSantos <112398203+MatheusMartinsDosSantos@users.noreply.github.com>
2023-11-29 15:44:06 -0300

    port

--- api/db.js
+++ api/db.js
@@ -2,6 +2,7 @@ import mysql from "mysql"
 
 export const db = mysql.createConnection({
     host: "20.195.166.255",
+    port: "3306",
     user: "root",
     password: "P@$$w0rd",
     database: "senac2303",
